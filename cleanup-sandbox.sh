4df216d089ad31c1e9e84842d204a1a7473c8160
Chris Chase <cchase@redhat.com>
2021-09-10 22:21:31 -0400

    Increase account duration and memory resources.

--- cleanup-sandbox.sh
+++ cleanup-sandbox.sh
@@ -4,6 +4,8 @@ echo "Make sure you are logged in as cluster admin"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+oc project toolchain-host-operator
+
 oc process -f "${DIR}/sandbox/nstemplatetiers/rhods-base.yaml" | oc delete -n toolchain-host-operator -f -
 oc process -f "${DIR}/sandbox/nstemplatetiers/rhods-baseextended.yaml" | oc delete -n toolchain-host-operator -f -
 oc process -f "${DIR}/sandbox/nstemplatetiers/rhods-basedeactivationdisabled.yaml" | oc delete -n toolchain-host-operator -f -

b72cc9962782c9bdffd0323942c78aed76765d5e
Chris Chase <cchase@redhat.com>
2021-09-16 17:18:02 -0400

    Updated dashboard deployment name. Use rhods-jupyterhub-sizes for Default notebook.

--- cleanup-rhods.sh
+++ cleanup-rhods.sh
@@ -7,10 +7,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 oc process -f "${DIR}/rhods/jupyterhub-idle-culler.yaml" | oc delete -n redhat-ods-applications -f -
 
 oc delete configmap odh-dashboard-config -n redhat-ods-applications
-oc rollout restart deployment/odh-dashboard -n redhat-ods-applications
+oc rollout restart deployment/rhods-dashboard -n redhat-ods-applications
 
 oc patch configmap rhods-groups-config -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-groups-config-original-patch.yaml"
-oc patch configmap jupyter-singleuser-profiles -n redhat-ods-applications --patch-file "${DIR}/rhods/jupyter-singleuser-profiles-original-patch.yaml"
 oc patch configmap rhods-jupyterhub-sizes -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-jupyterhub-sizes-original-patch.yaml"
 oc rollout latest deploymentconfig/jupyterhub -n redhat-ods-applications
 
--- setup-rhods.sh
+++ setup-rhods.sh
@@ -9,11 +9,10 @@ oc process -n redhat-ods-applications -f "${DIR}/rhods/jupyterhub-idle-culler.ya
   | oc apply -n redhat-ods-applications -f -
 
 oc apply -n redhat-ods-applications -f "${DIR}/rhods/odh-dashboard-config.yaml"
-oc rollout restart deployment/odh-dashboard -n redhat-ods-applications
+oc rollout restart deployment/rhods-dashboard -n redhat-ods-applications
 
 oc adm groups new rhods-admins || echo "rhods-admins group already exists"
 oc patch configmap rhods-groups-config -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-groups-config-patch.yaml"
-oc patch configmap jupyter-singleuser-profiles -n redhat-ods-applications --patch-file "${DIR}/rhods/jupyter-singleuser-profiles-patch.yaml"
 oc patch configmap rhods-jupyterhub-sizes -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-jupyterhub-sizes-patch.yaml"
 oc rollout latest deploymentconfig/jupyterhub -n redhat-ods-applications
 

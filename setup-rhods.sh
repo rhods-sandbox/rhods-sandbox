c8ac040bd24aaf15d50a3d09484fa9cf1159bdbf
Chris Chase <cchase@redhat.com>
2021-11-02 10:42:42 -0400

    Set rhods-admins to cluster-admins

--- setup-rhods.sh
+++ setup-rhods.sh
@@ -11,7 +11,6 @@ oc process -n redhat-ods-applications -f "${DIR}/rhods/jupyterhub-idle-culler.ya
 oc apply -n redhat-ods-applications -f "${DIR}/rhods/odh-dashboard-config.yaml"
 oc rollout restart deployment/rhods-dashboard -n redhat-ods-applications
 
-oc adm groups new rhods-admins || echo "rhods-admins group already exists"
 oc patch configmap rhods-groups-config -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-groups-config-patch.yaml"
 oc patch configmap rhods-jupyterhub-sizes -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-jupyterhub-sizes-patch.yaml"
 oc patch configmap odh-jupyterhub-global-profile -n redhat-ods-applications --patch-file "${DIR}/rhods/odh-jupyterhub-global-profile-patch.yaml"

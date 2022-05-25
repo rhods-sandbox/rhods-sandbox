21d350e86a1265e8b3c1919322d424c0c9ca15fe
Chris Chase <cchase@redhat.com>
2022-05-25 15:52:56 -0400

    Dynamic CSV read on install wait. Cleanup jsonpath. Use installedCSV instead of currentCSV.

--- cleanup-intel.sh
+++ cleanup-intel.sh
@@ -8,17 +8,17 @@ OPENVINO_NOTEBOOK=$(oc get Notebook -n redhat-ods-applications -o name)
 echo "deleting Openvino Notebook resource"
 oc delete $OPENVINO_NOTEBOOK -n redhat-ods-applications
 
-OVMS_CSV=$(oc get subscription ovms-operator -n openshift-operators -o go-template='{{.status.currentCSV}}')
+OVMS_CSV=$(oc get subscription ovms-operator -n openshift-operators -o jsonpath='{.status.installedCSV}')
 echo "deleting sub ovms-operator"
 echo "deleting csv ${OVMS_CSV}"
 oc delete subscription ovms-operator -n openshift-operators
 oc delete clusterserviceversion ${OVMS_CSV} -n openshift-operators
 
 AIKIT_CONTAINER_NAME=$(oc get AIKitContainer -n redhat-ods-applications -o name)
-echo "deleteing AIKit Container resource"
+echo "deleting AIKit Container resource"
 oc delete $AIKIT_CONTAINER_NAME -n redhat-ods-applications
 
-AIKIT_CSV=$(oc get subscription aikit-operator -n openshift-operators -o json)
+AIKIT_CSV=$(oc get subscription aikit-operator -n openshift-operators -o jsonpath='{.status.installedCSV}')
 echo "deleting sub aikit-operator"
 echo "deleting csv ${AIKIT_CSV}"
 oc delete subscription aikit-operator -n openshift-operators

45298d93b2f598bf86ebd49a0eb77093bc2705f9
Christopher Chase <cchase@redhat.com>
2022-06-28 12:54:19 -0400

    Update RHODS setup for culler and groups changes. (#10)

--- cleanup-rhods.sh
+++ cleanup-rhods.sh
@@ -4,14 +4,13 @@ echo "Make sure you are logged in as cluster admin"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-oc process -f "${DIR}/rhods/jupyterhub-idle-culler.yaml" | oc delete -n redhat-ods-applications -f -
-
-oc delete configmap odh-dashboard-config -n redhat-ods-applications
-oc rollout restart deployment/rhods-dashboard -n redhat-ods-applications
-
+oc patch configmap jupyterhub-cfg -n redhat-ods-applications --patch-file "${DIR}/rhods/jupyterhub-cfg-original-patch.yaml"
 oc patch configmap rhods-groups-config -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-groups-config-original-patch.yaml"
 oc patch configmap rhods-jupyterhub-sizes -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-jupyterhub-sizes-original-patch.yaml"
 oc patch configmap odh-jupyterhub-global-profile -n redhat-ods-applications --patch-file "${DIR}/rhods/odh-jupyterhub-global-profile-original-patch.yaml"
 oc rollout latest deploymentconfig/jupyterhub -n redhat-ods-applications
+
+oc delete configmap odh-dashboard-config -n redhat-ods-applications
+oc rollout restart deployment/rhods-dashboard -n redhat-ods-applications
 
 oc delete -f "${DIR}/rhods/cleanup-sandbox-user.cronjob.yaml"
--- setup-rhods.sh
+++ setup-rhods.sh
@@ -4,16 +4,13 @@ echo "Make sure you are logged in as cluster admin"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-oc process -n redhat-ods-applications -f "${DIR}/rhods/jupyterhub-idle-culler.yaml" \
-  -p IDLE_CULLER_TIMEOUT="${IDLE_CULLER_TIMEOUT}" \
-  | oc apply -n redhat-ods-applications -f -
-
-oc apply -n redhat-ods-applications -f "${DIR}/rhods/odh-dashboard-config.yaml"
-oc rollout restart deployment/rhods-dashboard -n redhat-ods-applications
-
+oc patch configmap jupyterhub-cfg -n redhat-ods-applications --patch-file "${DIR}/rhods/jupyterhub-cfg-patch.yaml"
 oc patch configmap rhods-groups-config -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-groups-config-patch.yaml"
 oc patch configmap rhods-jupyterhub-sizes -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-jupyterhub-sizes-patch.yaml"
 oc patch configmap odh-jupyterhub-global-profile -n redhat-ods-applications --patch-file "${DIR}/rhods/odh-jupyterhub-global-profile-patch.yaml"
 oc rollout latest deploymentconfig/jupyterhub -n redhat-ods-applications
+
+oc apply -n redhat-ods-applications -f "${DIR}/rhods/odh-dashboard-config.yaml"
+oc rollout restart deployment/rhods-dashboard -n redhat-ods-applications
 
 oc apply -f "${DIR}/rhods/cleanup-sandbox-user.cronjob.yaml"

4f555fa865ad35be3d22dd83f10d5902b93c50e7
Chris Chase <cchase@redhat.com>
2022-09-12 21:00:32 -0400

    Update culler, cleanup, and dashboard settings for RHODS 1.16

--- setup-rhods.sh
+++ setup-rhods.sh
@@ -4,12 +4,7 @@ echo "Make sure you are logged in as cluster admin"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-oc patch configmap jupyterhub-cfg -n redhat-ods-applications --patch-file "${DIR}/rhods/jupyterhub-cfg-patch.yaml"
-oc patch configmap rhods-groups-config -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-groups-config-patch.yaml"
-oc patch configmap rhods-jupyterhub-sizes -n redhat-ods-applications --patch-file "${DIR}/rhods/rhods-jupyterhub-sizes-patch.yaml"
-oc patch configmap odh-jupyterhub-global-profile -n redhat-ods-applications --patch-file "${DIR}/rhods/odh-jupyterhub-global-profile-patch.yaml"
-oc rollout latest deploymentconfig/jupyterhub -n redhat-ods-applications
-
+oc apply -n redhat-ods-applications -f "${DIR}/rhods/notebook-controller-culler-config.yaml"
 oc apply -n redhat-ods-applications -f "${DIR}/rhods/odh-dashboard-config.yaml"
 oc rollout restart deployment/rhods-dashboard -n redhat-ods-applications
 
